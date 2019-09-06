bed678c969a8c6c0cec4c8d261f12fd22ae03e4a
mfaria <mfaria@fazenda>
2019-09-06 12:44:47 -0400

    Implementação da tela de informações do projeto e ajuste no footer.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {ConverterComponent} from './converter/converter.component';
 import {PlayerComponent} from './player/player.component';
+import {AboutComponent} from "./about/about.component";
 
 const routes: Routes = [
   { path: '', redirectTo: 'converter', pathMatch: 'full' },
   { path: 'converter', component: ConverterComponent },
   { path: 'player', component: PlayerComponent },
+  { path: 'about', component: AboutComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { UrlComponent } from './url/url.component';
 import {ConverterService} from './shared/services/converter.service';
 import {HttpClientModule} from '@angular/common/http';
 import { PlayerComponent } from './player/player.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
@@ -24,7 +25,8 @@ import { PlayerComponent } from './player/player.component';
     UploadComponent,
     CardComponent,
     UrlComponent,
-    PlayerComponent
+    PlayerComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
