201f3450a010b79ff359f00e4198efb8a1b847c3
Magnum Adelano <magnemcs@gmail.com>
2019-09-07 23:21:45 -0400

    Configurando ambiente de produção

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
+const API = 'http://ec2-54-234-140-160.compute-1.amazonaws.com:8080';
+
 export const environment = {
-  production: true
+  production: true,
+  converter: {
+    file: API + '/converter/anytowebfile',
+    url: API + '/converter/anytoweburl',
+  },
+  bitmovin_apikey: 'bf6ef996-8bd8-4429-bded-9d49bbcd83f7'
 };
